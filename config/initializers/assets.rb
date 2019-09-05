078c669049c2888502cb09c5927a7f5274f52e13
Emerson2019 <xinayespinoza@gmail.com>
2019-09-05 02:05:38 -0400

    10 columnas, nuevo layout, navbar fixed, yield con clase container,background-color distintos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << Rails.root.join('vendor')
-
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap')
+Rails.application.config.assets.paths << Rails.root.join('vendor','bootstrap')
